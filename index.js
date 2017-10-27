cd68a0b02c202155e9f254f52cfbe9b88a001348
JWTappert <tappertj@oregonstate.edu>
2017-10-26 20:39:47 -0700

    Adds statis content in /public

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.render("index");
 })
 
 app.listen(process.env.PORT || 3000, function () {
-console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })
