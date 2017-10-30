0f3a43f2daba74c6d9b169f80ccaea92aa7be4e1
JWTappert <tappertj@oregonstate.edu>
2017-10-29 20:42:19 -0700

    Adds Sequelize, working on migrations

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/dio');
+
+sequelize
+    .authenticate()
+    .then(function() {
+    console.log('Connection has been established successfully.');
+})
+.catch(function(err) {
+    console.error('Unable to connect to the database:', err);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
@@ -9,8 +20,8 @@ app.set('views', path.resolve(__dirname, 'views'));
 
 app.get('/', function (req, res) {
     res.render("index");
-})
+});
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
+});
