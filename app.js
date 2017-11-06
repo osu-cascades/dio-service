6e3828ba183fcf8a355e8e72907eb5134b9bc131
JWTappert <tappertj@oregonstate.edu>
2017-11-05 20:15:30 -0800

    Adds logging with Morgan, Fixes #17

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_CONN);
-const api = require('./routes/api.js');
+const dotenv        = require('dotenv').config();
+const express       = require('express');
+const logger        = require('morgan');
+const path          = require('path');
+const bodyParser    = require('body-parser');
+const Sequelize     = require('sequelize');
+const api           = require('./routes/api.js');
 
-sequelize
-    .authenticate()
-    .then(function() {
+const app = express();
+const sequelize = new Sequelize(process.env.DB_CONN);
+
+sequelize.authenticate().then(function () {
     console.log('Connection has been established successfully.');
-})
-.catch(function(err) {
+}).catch((err) => {
     console.error('Unable to connect to the database:', err);
 });
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v1', api);
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
--- dist/app.js
+++ dist/app.js
@@ -2,12 +2,14 @@
 
 var dotenv = require('dotenv').config();
 var express = require('express');
+var logger = require('morgan');
 var path = require('path');
 var bodyParser = require('body-parser');
-var app = express();
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(process.env.DB_CONN);
 var api = require('./routes/api.js');
+
+var app = express();
+var sequelize = new Sequelize(process.env.DB_CONN);
 
 sequelize.authenticate().then(function () {
     console.log('Connection has been established successfully.');
@@ -15,12 +17,13 @@ sequelize.authenticate().then(function () {
     console.error('Unable to connect to the database:', err);
 });
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v1', api);
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
