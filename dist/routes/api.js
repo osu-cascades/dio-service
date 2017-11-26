a180f459fbcb9b1a8525376af5d786d31b971cc2
JWTappert <tappertj@oregonstate.edu>
2017-11-26 13:57:12 -0800

    Adds additional route, fixes #19

--- dist/routes/api.js
+++ dist/routes/api.js
@@ -4,18 +4,37 @@ var express = require('express');
 var models = require('../models');
 var api = express.Router();
 
-api.post("/send", function (req, res) {
-   models.DissolvedOxygenReading.create({
-      reading: req.body.reading,
-      location: req.body.location
-   }).then(res.status(201));
+api.post('/do', function (req, res) {
+    models.DissolvedOxygenReading.create({
+        reading: req.body.reading,
+        location: req.body.location
+    }).then(function () {
+        res.status(200);
+        res.send('Success');
+    }).catch(function (err) {
+        return console.error(err);
+    });
 });
 
-api.get("/list", function (req, res) {
-   models.DissolvedOxygenReading.findAll().then(function (result) {
-      res.status(200);
-      res.send(result);
-   });
+api.get('/do/last', function (req, res) {
+    models.DissolvedOxygenReading.findOne({
+        limit: 1,
+        order: [['createdAt', 'DESC']]
+    }).then(function (reading) {
+        res.status(200);
+        res.send(reading);
+    }).catch(function (err) {
+        return console.log(err);
+    });
+});
+
+api.get('/do/all', function (req, res) {
+    models.DissolvedOxygenReading.findAll().then(function (result) {
+        res.status(200);
+        res.send(result);
+    }).catch(function (err) {
+        return console.log(err);
+    });
 });
 
 module.exports = api;
--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,32 @@ const express     = require('express');
 const models      = require('../models');
 const api         = express.Router();
 
-api.post("/send", (req, res) => {
+
+api.post('/do', (req, res) => {
     models.DissolvedOxygenReading.create({
        reading: req.body.reading,
        location: req.body.location
-   }).then(res.status(201));
+    }).then(() => {
+        res.status(200);
+        res.send(`Success`);
+    }).catch(err => console.error(err));
 });
 
-api.get("/list", function(req, res) {
+api.get('/do/last', (req, res) => {
+    models.DissolvedOxygenReading.findOne({
+        limit: 1,
+        order: [ [ 'createdAt', 'DESC' ] ]
+    }).then(reading => {
+        res.status(200);
+        res.send(reading);
+    }).catch(err => console.log(err));
+});
+
+api.get('/do/all', function(req, res) {
    models.DissolvedOxygenReading.findAll().then(result => {
        res.status(200);
        res.send(result);
-   });
+   }).catch(err => console.log(err));
 });
 
 module.exports = api;
