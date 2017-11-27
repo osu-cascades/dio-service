0026685f4e71ffe88c49e3ea97929c3c63a63eab
JWTappert <tappertj@oregonstate.edu>
2017-11-26 19:37:56 -0800

    Changes API to adhere to the JSON API standard.

--- dist/routes/api.js
+++ dist/routes/api.js
@@ -4,7 +4,7 @@ var express = require('express');
 var models = require('../models');
 var api = express.Router();
 
-api.post('/do', function (req, res) {
+api.post('/do/readings', function (req, res) {
     models.DissolvedOxygenReading.create({
         reading: req.body.reading,
         location: req.body.location
@@ -16,22 +16,10 @@ api.post('/do', function (req, res) {
     });
 });
 
-api.get('/do/last', function (req, res) {
-    models.DissolvedOxygenReading.findOne({
-        limit: 1,
-        order: [['createdAt', 'DESC']]
-    }).then(function (reading) {
+api.get('/do/readings', function (req, res) {
+    models.DissolvedOxygenReading.findAll().then(function (readings) {
         res.status(200);
-        res.send(reading);
-    }).catch(function (err) {
-        return console.log(err);
-    });
-});
-
-api.get('/do/all', function (req, res) {
-    models.DissolvedOxygenReading.findAll().then(function (result) {
-        res.status(200);
-        res.send(result);
+        res.send(readings);
     }).catch(function (err) {
         return console.log(err);
     });
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const models      = require('../models');
 const api         = express.Router();
 
 
-api.post('/do', (req, res) => {
+api.post('/do/readings', (req, res) => {
     models.DissolvedOxygenReading.create({
        reading: req.body.reading,
        location: req.body.location
@@ -13,21 +13,11 @@ api.post('/do', (req, res) => {
     }).catch(err => console.error(err));
 });
 
-api.get('/do/last', (req, res) => {
-    models.DissolvedOxygenReading.findOne({
-        limit: 1,
-        order: [ [ 'createdAt', 'DESC' ] ]
-    }).then(reading => {
-        res.status(200);
-        res.send(reading);
-    }).catch(err => console.log(err));
-});
-
-api.get('/do/all', function(req, res) {
-   models.DissolvedOxygenReading.findAll().then(result => {
+api.get('/do/readings', function(req, res) {
+    models.DissolvedOxygenReading.findAll().then(readings => {
        res.status(200);
-       res.send(result);
-   }).catch(err => console.log(err));
+       res.send(readings);
+    }).catch(err => console.log(err));
 });
 
 module.exports = api;
