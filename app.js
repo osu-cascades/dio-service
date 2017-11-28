b157548e3ab0bee1bc3033a5c228a91836dab644
JWTappert <tappertj@oregonstate.edu>
2017-11-27 18:53:07 -0800

    Enables CORS on our API

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api/v1', api);
 app.use((req, res) => {
     res.status(400);
--- dist/app.js
+++ dist/app.js
@@ -23,6 +23,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api/v1', api);
 app.use(function (req, res) {
     res.status(400);
