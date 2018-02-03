2fb26b0a2dd30b7abb0b4563af28bc90abf43fb8
Justin Tappert <justin@theflybook.com>
2018-02-02 23:51:57 -0800

    Adds end point for querying a date range

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const models = require('../models');
 const api = express.Router();
+const moment = require('moment');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // save new reading to the database
 api.post('/do/readings', (req, res) => {
@@ -51,12 +54,32 @@ api.get('/do/readings/last', (req, res) => {
     })
         .then(reading => {
             res.status(200);
-            res.send(reading)
+            res.send(reading);
         })
         .catch(error => {
             res.status(500);
             res.send(error);
         });
+});
+
+api.post('/do/readings/query', (req, res) => {
+    let start = moment(req.startDate).format('YYYY-MM-DD HH:mm:ss');
+    let end = moment(req.endDate).format('YYYY-MM-DD HH:mm:ss');
+    models.DissolvedOxygenReading.findAll({
+        where: {
+            createdAt: {
+                [Op.between]: [start, end]
+            }
+        }
+    })
+        .then(readings => {
+            res.status(200);
+            res.send(readings);
+        })
+        .catch(errors => {
+            res.status(400);
+            res.send(errors);
+        })
 });
 
 module.exports = api;
