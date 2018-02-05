04cc82adfa0b6bc77c2e1b49decccf94dea9d857
Justin Tappert <justin@theflybook.com>
2018-02-04 16:29:30 -0800

    Reduce threshold to 9

--- public/javascript/twilio.js
+++ public/javascript/twilio.js
@@ -6,15 +6,19 @@ const client = new twilio(accountSid, authToken);
 
 const twilioEvent = {
     eventFilter: (reading) => {
-        if (reading <= 10) {
-            client.messages.create({
-                body: 'The Dissolved Oxygen reading fell below 10ppm',
-                to: process.env.recipient,
-                from: process.env.sender
-            })
-                .then((message) => console.log("Text message sent: id:" + message.sid))
-                .catch((errors) => console.log("Failed to send text: " + errors));
+        if (reading < 9) {
+            this.sendNotification();
         }
+    },
+
+    sendNotification: () => {
+        client.messages.create({
+            body: 'The Dissolved Oxygen reading fell below 10ppm',
+            to: process.env.recipient,
+            from: process.env.sender
+        })
+            .then((message) => console.log("Text message sent: id:" + message.sid))
+            .catch((errors) => console.log("Failed to send text: " + errors));
     }
 };
 
