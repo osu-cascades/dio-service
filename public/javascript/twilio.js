a0dfbb82cab95b438eaa0d957cb796e47f1de628
JWTappert <justin@theflybook.com>
2018-05-18 07:35:09 -0700

    fixing the twilio service

--- public/javascript/twilio.js
+++ public/javascript/twilio.js
@@ -1,25 +1,26 @@
-const twilio = require('twilio');
+const twilio = require("twilio");
 const accountSid = process.env.accountSid;
 const authToken = process.env.authToken;
 
 const client = new twilio(accountSid, authToken);
 
 const twilioEvent = {
-    eventFilter: (reading) => {
-        if (reading < 5) {
-            this.sendNotification();
-        }
-    },
+	eventFilter(reading) {
+		if (reading < 5) {
+			this.sendNotification();
+		}
+	},
 
-    sendNotification: function() {
-        client.messages.create({
-            body: 'The Dissolved Oxygen reading fell below 10ppm',
-            to: process.env.recipient,
-            from: process.env.sender
-        })
-            .then((message) => console.log("Text message sent: id:" + message.sid))
-            .catch((errors) => console.log("Failed to send text: " + errors));
-    }
+	sendNotification() {
+		client.messages
+			.create({
+				body: "The Dissolved Oxygen reading fell below 10ppm",
+				to: process.env.recipient,
+				from: process.env.sender
+			})
+			.then(message => console.log("Text message sent: id:" + message.sid))
+			.catch(errors => console.log("Failed to send text: " + errors));
+	}
 };
 
 module.exports = twilioEvent;
