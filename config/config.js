6a5be541e3b03ccca52a0050112ce85b7caccd81
JWTappert <justin@theflybook.com>
2018-05-20 13:01:03 -0700

    creates the different environment configs

--- config/config.js
+++ config/config.js
@@ -1,17 +1,67 @@
-const dotenv = require('dotenv').config();
-module.exports = {
-    development: {
-        username: process.env.DEV_USER,
-        password: process.env.DEV_PASS,
-        database: process.env.DEV_DB,
-        host: process.env.DEV_HOST,
-        dialect: 'mysql'
-    },
-    production: {
-        username: process.env.PROD_USER,
-        password: process.env.PROD_PASS,
-        database: process.env.PROD_DB,
-        host: process.env.PROD_HOST,
-        dialect: 'mysql'
-    }
+const dotenv = require("dotenv").config();
+const env = process.env.NODE_ENV || "dev";
+
+const dev = {
+	app: {
+		port: parseInt(process.env.DEV_APP_PORT) || 3000
+	},
+	db: {
+		username: process.env.DEV_USER,
+		password: process.env.DEV_PASS,
+		database: process.env.DEV_DB,
+		host: process.env.DEV_HOST,
+		dialect: "mysql"
+	},
+	twilio: {
+		accountSid: process.env.accountSid,
+		authToken: process.env.authToken,
+		sender: process.env.sender,
+		recipient: process.env.recipient
+	}
 };
+
+const prod = {
+	app: {
+		port: 3000
+	},
+	db: {
+		username: process.env.PROD_USER,
+		password: process.env.PROD_PASS,
+		database: process.env.PROD_DB,
+		host: process.env.PROD_HOST,
+		dialect: "mysql"
+	},
+	twilio: {
+		accountSid: process.env.accountSid,
+		authToken: process.env.authToken,
+		sender: process.env.sender,
+		recipient: process.env.recipient
+	}
+};
+
+const test = {
+	app: {
+		port: 3000
+	},
+	db: {
+		username: process.env.DEV_USER,
+		password: process.env.DEV_PASS,
+		database: process.env.DEV_DB,
+		host: process.env.DEV_HOST,
+		dialect: "mysql"
+	},
+	twilio: {
+		accountSid: process.env.accountSid,
+		authToken: process.env.authToken,
+		sender: process.env.sender,
+		recipient: process.env.recipient
+	}
+};
+
+const config = {
+	dev,
+	prod,
+	test
+};
+
+module.exports = config[env];
