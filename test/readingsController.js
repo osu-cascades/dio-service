db512b20389b8ccc47e4874d106a5c734830b6c5
JWTappert <justin@theflybook.com>
2018-05-26 18:37:07 -0700

    adding controller tests

--- test/readingsController.js
+++ test/readingsController.js
@@ -1,3 +1,36 @@
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+const Controller = require("../controllers/ReadingsController");
+
+describe("readingsController Tests", function() {
+	let controller = new Controller();
+	let save;
+	beforeEach(function() {
+		save = sinon.stub(controller, "saveReading");
+	});
+	it("saves the new reading", function() {
+		let spy = sinon.spy(controller, "handleReading");
+
+		controller.handleReading(4.44, "home", 0);
+
+		sinon.assert.calledOnce(spy);
+		sinon.assert.calledWith(save, 4.44, "home", 0);
+	});
+
+	it("throws an error if reading is non-numeric", function() {
+		let spy = sinon.spy(controller, "ensureReadingDataIsNumeric");
+
+		controller.handleReading(4.44, "home", 0);
+
+		sinon.assert.calledOnce(spy);
+	});
+
+	afterEach(function() {
+		save.restore();
+	});
+});
+
 const fakeData = [
 	{
 		id: 37,
@@ -28,6 +61,14 @@ const fakeData = [
 		reading: 5.67,
 		location: "home",
 		type: 1,
+		createdAt: "2018-05-21T01:28:44.000Z",
+		updatedAt: "2018-05-21T01:28:44.000Z"
+	},
+	{
+		id: 41,
+		reading: 4.44,
+		location: "home",
+		type: 0,
 		createdAt: "2018-05-21T01:28:44.000Z",
 		updatedAt: "2018-05-21T01:28:44.000Z"
 	}
